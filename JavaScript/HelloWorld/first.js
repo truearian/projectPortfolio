aebad1facf22215fb763fc9d9e720795d042b5d1
truearian <139094765+truearian@users.noreply.github.com>
2023-09-04 12:49:24 -0400

    Add files via upload
    some javascript practice I was doing over the summer

---
+++ JavaScript/HelloWorld/first.js
@@ -0,0 +1,11 @@
+let admin;
+let password;
+let secure = false;
+
+admin = "admin";
+password = "password";
+
+if (admin = "admin") secure = true;
+
+if(secure) alert("admin is secured");
+else alert("admin is not secured");
